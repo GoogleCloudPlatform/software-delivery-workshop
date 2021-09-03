8fe03197e5263b7b912f058030a8059903764136
gushob <gushob@google.com>
2021-09-03 15:03:35 +0000

    Incorporating cloud deploy

--- delivery-platform/scripts/app.sh
+++ delivery-platform/scripts/app.sh
@@ -144,10 +144,6 @@ delete () {
         rm -rf $WORK_DIR/acm-repo
    elif [[ ${CONTINUOUS_DELIVERY_SYSTEM} == "Clouddeploy" ]]; then
         #Delete the deployments for dev, staging and prod. The deployments with CD are created with default namespace
-        kubectx dev && kubectl get deploy --namespace default --selector="app=${APP_NAME}" --output jsonpath='{.items[0].metadata.name}' || true
-        kubectx stage && kubectl get deploy --namespace default --selector="app=${APP_NAME}" --output jsonpath='{.items[0].metadata.name}' || true
-        kubectx prod && kubectl get deploy --namespace default --selector="app=${APP_NAME}" --output jsonpath='{.items[0].metadata.name}' || true
-        echo "##############HIGHLIGHTS##################"
         kubectx dev && kubectl delete deploy $(kubectl get deploy --namespace default --selector="app=${APP_NAME}" --output jsonpath='{.items[0].metadata.name}') || true
         kubectx stage && kubectl delete deploy $(kubectl get deploy --namespace default --selector="app=${APP_NAME}"  --output jsonpath='{.items[0].metadata.name}') || true
         kubectx prod && kubectl delete deploy $(kubectl get deploy --namespace default --selector="app=${APP_NAME}"  --output jsonpath='{.items[0].metadata.name}') || true
