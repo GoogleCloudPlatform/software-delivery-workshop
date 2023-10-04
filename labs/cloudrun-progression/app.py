b24a22cc5d38e06e74519dbbc36e85870fd16af8
beardedsamwise <sam.bentley@withriley.com>
2023-10-04 00:54:47 +0000

    updated

--- labs/cloudrun-progression/app.py
+++ labs/cloudrun-progression/app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World v1.0'
+    return 'Hello World v1.1'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
